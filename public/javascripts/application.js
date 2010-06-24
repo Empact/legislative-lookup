26d6527053e95aab2a071e3a445632b035430375
Ben Woosley <ben.woosley@gmail.com>
2010-06-23 21:00:00 -0400

    Move lookup js out of the view and into application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,10 +1,29 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-
 function togglePolygon(polygon) {
   if (polygon.isHidden()) {
     polygon.show();
   } else {
     polygon.hide();
   }
+}
+
+var geocoder = new GClientGeocoder();
+function geocodeAddress(address) {
+  geocoder.getLatLng(
+    address,
+    function(point) {
+      if (!point) {
+        alert(address + " not found");
+      } else {
+        $('address_span').hide();
+        $('lat_lng').show();
+        $('lat').value = point.lat();
+        $('lng').value = point.lng();
+      }
+    }
+  );
+}
+
+function clear_box(){
+  $('address_span').show();
+  $('lat_lng').hide();
 }
